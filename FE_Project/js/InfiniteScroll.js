0dc771659149a67ffab71638b72a48c897566789
Allewis <allewis0201@gmail.com>
2024-01-22 18:19:24 +0900

    Feat: 코드 수정 및 추가
    FE_Project.html 수정
    로고에 링크 추가
    
    hoduStyle.css 수정
    img-list 스타일 수정(object-fit : cover를 추가해 사진들이 찌그러지지않게 방지함)
    
    infiniteScroll.js 수정
    기존 이미지 api에서 웹페이지의 컨셉에 맞도록 고양이 이미지 api로 교체
    
    map.js 수정
    맵을 누르면 마커가 이동 가능하도록 함
    마커가 이동하면 마커의 주소를 표시하도록 함

--- FE_Project/js/InfiniteScroll.js
+++ FE_Project/js/InfiniteScroll.js
@@ -27,7 +27,7 @@ window.addEventListener("scroll",() =>{
 
 async function makeImages(pageNum){
     try {
-        const response = await fetch(`https://picsum.photos/v2/list?page=${pageNum}&limit=10`);
+        const response = await fetch(`https://cataas.com/api/cats?limit=1&skip=${pageNum}`);
         if (!response.ok) {
             throw new Error('네트워크 응답에 문제가 있습니다.');
         }
@@ -45,8 +45,7 @@ function makeImageList(datas)
 {
     datas.forEach((item) => {
         /*imageList.innerHTML += `<img src = "${item.download_url}" class = "img-list" alt = '' ></li>`*/
-
-        imageList.insertAdjacentHTML('beforeend',`<img src = "${item.download_url}" class = "img-list" alt = '' ></li>`)
+        imageList.insertAdjacentHTML('beforeend',`<img src = "https://cataas.com/cat/${item._id}" class = "img-list" alt = '' ></li>`)
     });
 }
 
--- FE_Project/js/map.js
+++ FE_Project/js/map.js
@@ -21,16 +21,34 @@ var zoomControl = new kakao.maps.ZoomControl();
 map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
 // 마커가 표시될 위치를 선정함.
-var markerPosition  = new kakao.maps.LatLng(33.4423379727783, 126.571449734542);
+var startPosition  = new kakao.maps.LatLng(33.4423379727783, 126.571449734542);
 // 마커를 생성함.
 var marker = new kakao.maps.Marker({
-    position: markerPosition
+    position: startPosition
 });
 // 마커가 지도 위에 표시되도록 설정함.
 marker.setMap(map);
 
-/*
-let test= document.querySelector("#test");
 
-test.innerHTML = map.getCenter();
-*/
+// 맵 정보
+let mapLocation = document.querySelector("#p-address");
+// 주소-좌표 변환 객체 생성
+var geocoder = new kakao.maps.services.Geocoder();
+
+// 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록합니다
+kakao.maps.event.addListener(map, "click", function (mouseEvent) {
+    searchDetailAddrFromCoords(mouseEvent.latLng, function(result, status) {
+        if(status === kakao.maps.services.Status.OK) {
+            mapLocation.innerHTML = result[0].address.address_name;
+
+            marker.setPosition(mouseEvent.latLng);
+            marker.setMap(map);
+        }
+    });
+})
+
+
+function searchDetailAddrFromCoords(coords, callback) {
+    // geocoder 객체의 coord2Address()로 해당 좌표의 지번을 얻는다.
+    geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+}
